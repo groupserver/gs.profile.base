70d85ef9ce6373e0eeb6bdf84a7e1b584f185b61
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-29 16:48:46 -0700

    Cleaning up the Viewlet

--- gs/profile/base/viewlet.py
+++ gs/profile/base/viewlet.py
@@ -1,8 +1,8 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from contentprovider import ContentProvider
 
-class Viewlet(ContentProvider):
-    def __init__(self , user, request, view, manager):
-        ContentProvider.__init__(self , user, request, view)
-        self.manager = manager
 
+class Viewlet(ContentProvider):
+    def __init__(self, user, request, view, manager):
+        super(Viewlet, self).__init__(user, request, view)
+        self.manager = manager
