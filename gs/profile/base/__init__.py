64fd68bf48da4a097cf8cb6d0a942aa20caf0a5d
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-29 16:49:39 -0700

    Importing the ProfilePage, ProfileContentProvider, and ProfileViewlet.

---
+++ gs/profile/base/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#lint:disable
+from page import ProfilePage
+from contentprovider import ContentProvider as ProfileContentProvider
+from viewlet import Viewlet as ProfileViewlet
+#lint:enable
