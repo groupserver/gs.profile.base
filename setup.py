c22c9dc8558b43a41c7d306d1644f227488ee8a9
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-26 12:24:12 -0700

    Turning off zip_safe, and updating the egg deps.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,15 +24,15 @@ setup(name='gs.profile.base',
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
       ],
     keywords='profile groupserver viewlet',
     author='Michael JasonSmith',
@@ -29,17 +42,21 @@ setup(name='gs.profile.base',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.profile'],
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     install_requires=[
         'setuptools',
         'AccessControl',
         'zope.app.publisher',
+        'zope.browsermenu',
         'zope.cachedescriptors',
         'zope.component',
         'zope.contentprovider',
         'zope.interface',
         'zope.pagetemplate',
         'zope.schema',
+        'zope.tal',
+        'zope.tales',
+        'Zope2',
         'gs.content.base',
         'gs.content.form',
         'gs.viewlet',
